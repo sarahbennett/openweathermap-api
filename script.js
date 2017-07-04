b4c06bf633ed8c787a001e8963969adaa10b114d
sarahbennett <sarahclbs@gmail.com>
2017-07-04 15:06:21 -0400

    adds updated version with first draft of css and functioning my location button

--- script.js
+++ script.js
@@ -10,17 +10,19 @@ const coordinates = [
   {lat: 47.6762, lon: -122.3182} //seattle
 ]
 
-var weatherInfo = []
+function addCoords(){
+  navigator.geolocation.getCurrentPosition(geolocSuccess, geolocError);
+}
 
-  function buildWeatherReport() {
-      var info = document.forms[0];
-      var i;
-      for (i = 0; i < info.length; i++) {
-          if (info[i].checked) {
-            weatherInfo.push(info[i].value);
-          }
-      }
-  }
+function geolocSuccess(position){
+  const newPos = {lat: position.coords.latitude, lon: position.coords.longitude};
+  coordinates.push(newPos);
+}
+
+// callback for no success getting location from user's browser
+function geolocError(){
+  console.log("Error getting user's location :(");
+}
 
 function handleSubmit () {
   event.preventDefault()
@@ -28,23 +30,26 @@ function handleSubmit () {
   // get form values
   var cityChoice = form.selection.value
   if (form.selection.value === "London"){
-    var cityChoice = coordinates[0]
+    var cityChoice = coordinates[0];
+  } else if (form.selection.value === "Seattle") {
+    var cityChoice = coordinates[1];
   } else {
-    var cityChoice = coordinates[1]
+    var cityChoice = coordinates[2];
   }
   // serialize them into a query string
   let queryString = buildQuery(cityChoice)
   // call getWeather with the query string
   console.log(queryString)
-  buildWeatherReport()
   getWeather(queryString)
 }
+
 
 function buildQuery(cityChoice) {
   if (form.units.value == "Celcius"){
     return `${apiURL}?lat=${cityChoice.lat}&lon=${cityChoice.lon}&units=metric&APPID=${apiKey}`
   } else {
-    return `${apiURL}?lat=${cityChoice.lat}&lon=${cityChoice.lon}&units=imperial&APPID=${apiKey}`}
+    return `${apiURL}?lat=${cityChoice.lat}&lon=${cityChoice.lon}&units=imperial&APPID=${apiKey}`
+  }
 }
 
 
@@ -55,29 +60,34 @@ function getWeather (queryString) {
   // fires when the request is complete
   request.onload = function () {
     let response = JSON.parse(request.response);
+
+    var temp = response.main.temp
     let description = response.weather[0].description
-    let icon = response.weather[0].icon
-    let temp = response.main.temp+"\u00B0"
     let humidity = response.main.humidity+"%"
     let sunsetUnix = response.sys.sunset
     let sunsetTime = moment(sunsetUnix*1000).toString()
     let sunriseUnix = response.sys.sunrise
-    let sunriseTime = moment(sunsetUnix*1000).toString()
+    let sunriseTime = moment(sunriseUnix*1000).toString()
+
+    var weatherInfo = []
+
+    var info = document.forms[0].info;
+    var i;
+    for (i = 0; i < info.length; i++) {
+        if (info[i].checked) {
+        weatherInfo.push(info[i].value);
+        }
+      }
 
     let weatherDiv = document.getElementById("weather")
     var li = document.createElement("li");
-    var i=0;
     for (i = 0; i < weatherInfo.length; i++) {
-        if(weatherInfo[i] == "temperature"){
-          li.innerHTML = "The current " + weatherInfo[i] + " is" + temp
-        } else if (weatherInfo[i] == "weather overall"){
-          li.innerHTML = "General weather description: " + description
-        } else if (weatherInfo[i] == "humidity"){
-          li.innerHTML = "The current " + weatherInfo[i] + " is" + humidity
-        }
-        weatherDiv.appendChild(li)
+        li.innerHTML = eval(weatherInfo[i]);
+        weatherDiv.appendChild(li);
+        weatherInfo = []
       }
   }
+
   // fires if something goes wrong
   request.error = function (errorObject) {
     console.log("Sorry, there is an error")
@@ -86,7 +96,25 @@ function getWeather (queryString) {
   request.send()
 }
 
-// function weatherBuilder() {
+
+// tempEval = eval("response.main.temp")
+// console.log(tempEval+ "\u00B0")
+// function buildWeatherReport() {
+// for (i=0; i<weatherInfo.length; i++) {
+//   var infoValue = eval(weatherInfo[i])
+//   console.log(infoValue);
+// };
+//   var information = form.info
+//   var i;
+//   for (i = 0; i < information.length; i++) {
+//     if (information[i].checked){
+//       var infoValue = eval(information[i].value)
+//       console.log(infoValue);
+//     }
+//   }
+// }
+
+// functionweatherBuilder() {
 //   console.log("building the weather report!")
 //   event.preventDefault();
 //   var form = document.querySelector("form");
